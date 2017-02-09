08958f17d30233672f0098a02d6b3abdab3e6f02
Pierre Flaudias <piflaudias@dpt_info.iut.local>
2017-02-09 17:25:32 +0100

    update status list

--- app/templates/statusList.php
+++ app/templates/statusList.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Twitter</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	</head>
-	<body>
-		<div class="container">
-			<h1>Tweets</h1>
-			<table class="table table-bordered table-hover">
-				<?php
+<head>
+	<title>Twitter</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1>Tweets</h1>
+		<table class="table table-bordered table-hover">
+			<?php
 
-				foreach ($statuses as $status) {
-    var_dump($status);?>
-				    <tr><td><a href="/statuses/<?= $status->getId() ?>">Tweet <?= $status->getId() ?></a> : <?= $status->getContent() ?></td><small><?= $status->getCreatedAt() ?></small></tr>
-				<?php
-				}
-				?>
-			</table>
-			<form action="/statuses" method="POST" class="form-horizontal">
-				<div class="form-group">
-				    <label for="username">Username:
-				        <input type="text" name="username" class="form-control" required>
-				    </label>
-				 </div>
-				 <div class="form-group">
-				    <label for="message">Message :</label>
-				        <textarea name="message" class="form-control" required></textarea>
-				 </div>
-				    <input type="hidden" name="_method" value="POST">
-				    <button class="btn btn-info" type="submit">Tweet !</button>
-			</form>
-		</div>
-	</body>
+			foreach ($statuses as $status) {?>
+			<tr>
+				<td>
+					<a href="/statuses/<?= $status->getId() ?>">Tweet <?= $status->getId() ?></a> : <?= $status->getContent() ?>
+					<small><?= $status->getCreatedAt()->format('m/d/Y') ?></small> 
+				</td>
+			</tr>
+			<?php
+			}
+			?>
+		</table>
+		<form action="/statuses" method="POST" class="form-horizontal">
+			<div class="form-group">
+				<label for="username">Username:
+					<input type="text" name="username" class="form-control" required>
+				</label>
+			</div>
+			<div class="form-group">
+				<label for="message">Message :</label>
+				<textarea name="message" class="form-control" required></textarea>
+			</div>
+			<input type="hidden" name="_method" value="POST">
+			<button class="btn btn-info" type="submit">Tweet !</button>
+		</form>
+	</div>
+</body>
 </html>
--- src/Dal/Json/JsonFinder.php
+++ src/Dal/Json/JsonFinder.php
@@ -1,20 +1,22 @@
 <?php
 
-namespace DAL\Json;
+namespace Dal\Json;
 
-class JsonFinder implements \DAL\FinderInterface{
+use Dal\FinderInterface;
+
+class JsonFinder implements FinderInterface {
 	
 	public function findAll(){
 		return json_decode(file_get_contents(__DIR__ . '../../../../data/statuses.json'));
 	}
 	
 	public function findOneById($id){
-	    $statuses = json_decode(file_get_contents(__DIR__ . '../../../../data/statuses.json'));
+		$statuses = json_decode(file_get_contents(__DIR__ . '../../../../data/statuses.json'));
 		foreach ($statuses as $status){
-		    if($status->id == $id){
-		        return $status;
-            }
-        }
-        return null;
+			if($status->id == $id){
+				return $status;
+			}
+		}
+		return null;
 	}
 }
